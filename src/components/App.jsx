10d5d6e71e2cd7af94720b43f0ce08823738c274
Kyle Morgan <kmorgan8588@gmail.com>
2020-06-16 15:37:48 -0700

    adding basic files to outline the react app

---
+++ src/components/App.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const App = (props) => <div>Hello world from App</div>;
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { Render } from "react-dom";
+import App from "./components/App";
+
+Render(<App />, document.getElementById("root"));
